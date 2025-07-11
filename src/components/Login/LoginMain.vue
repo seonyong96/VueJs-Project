f3d1abbdd5c125adae3ce98c03611b3267b09d62
yongJun Park <dydwnsl02@naver.com>
2025-07-11 11:47:39 +0900

    test

--- src/components/Login/LoginMain.vue
+++ src/components/Login/LoginMain.vue
@@ -18,7 +18,7 @@ const handlerLogin = () => {
       sessionStorage.setItem('userInfo', JSON.stringify(data));
       setUserData(data);
       router.push('/vue');
-    } else {
+    } else { 
       alert('아이디 혹은 비밀번호가 일치하지 않아요');
       return;
     }
